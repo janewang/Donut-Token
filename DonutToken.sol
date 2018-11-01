a9887ca646731be09aa861650d46511f599472d1
Jane Wang <janelwang2008@gmail.com>
2018-10-31 22:34:42 -0400

    Use safe math

--- DonutToken.sol
+++ DonutToken.sol
@@ -13,20 +13,43 @@ pragma solidity ^0.4.18;
  */
 
 contract ERC20Interface {
-  uint public totalSupply;
-  function balanceOf(address _owner) public constant returns (uint balance);
-  function transfer(address _to, uint _value) public returns (bool success);
-  function allowance(address _owner, address _spender) public constant returns (uint remaining);
-  function approve(address _spender, uint _value) public returns (bool success);
+    uint public totalSupply;
+    function balanceOf(address _owner) public constant returns (uint balance);
+    function transfer(address _to, uint _value) public returns (bool success);
+    function allowance(address _owner, address _spender) public constant returns (uint remaining);
+    function approve(address _spender, uint _value) public returns (bool success);
 
-  event Transfer(address indexed _from, address indexed _to, uint _value);
-  event Approval(address indexed _owner, address indexed _spender, uint _value);
+    event Transfer(address indexed _from, address indexed _to, uint _value);
+    event Approval(address indexed _owner, address indexed _spender, uint _value);
+}
+
+/**
+ * Safe maths
+ */
+library SafeMath {
+    function add(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a + b;
+        require(c >= a);
+    }
+    function sub(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b <= a);
+        c = a - b;
+    }
+    function mul(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        c = a * b;
+        require(a == 0 || c / a == b);
+    }
+    function div(uint256 a, uint256 b) internal pure returns (uint256 c) {
+        require(b > 0);
+        c = a / b;
+    }
 }
 
 /**
  * @title Standard ERC20 token
  */
 contract ERC20StandardToken is ERC20Interface {
+    using SafeMath for uint256;
 
     mapping (address => uint256) balances;
     mapping (address => mapping (address => uint256)) allowed;
@@ -37,11 +60,10 @@ contract ERC20StandardToken is ERC20Interface {
      * @param _value The amount to be transferred.
      */
     function transfer(address _to, uint256 _value) public returns (bool success) {
-	    require((_value > 0) && (balances[msg.sender] >= _value));
-	    balances[msg.sender] -= _value;
-    	balances[_to] += _value;
-    	emit Transfer(msg.sender, _to, _value);
-    	return true;
+        balances[msg.sender] = balances[msg.sender].sub(_value);
+        balances[_to] = balances[_to].add(_value);
+      	emit Transfer(msg.sender, _to, _value);
+      	return true;
     }
 
     /**
@@ -49,30 +71,30 @@ contract ERC20StandardToken is ERC20Interface {
      * @param _owner The address to query the the balance of.
      * @return An uint256 representing the amount owned by the passed address.
      */
-    function balanceOf(address _owner) public constant returns (uint256 balance) {
+    function balanceOf(address _owner) public view returns (uint256 balance) {
         return balances[_owner];
     }
 
     /**
-     * @dev Aprove the passed address to spend the specified amount of tokens on behalf of msg.sender.
+     * @dev Approve the passed address to spend the specified amount of tokens on behalf of msg.sender.
      * @param _spender The address which will spend the funds.
      * @param _value The amount of tokens to be spent.
      */
     function approve(address _spender, uint256 _value) public returns (bool success) {
         require((_value == 0) || (allowed[msg.sender][_spender] == 0));
-    	allowed[msg.sender][_spender] = _value;
-    	Approval(msg.sender, _spender, _value);
-    	return true;
+      	allowed[msg.sender][_spender] = _value;
+        emit Approval(msg.sender, _spender, _value);
+      	return true;
     }
 
     /**
      * @dev Function to check the amount of tokens that an owner allowed to a spender.
      * @param _owner address The address which owns the funds.
      * @param _spender address The address which will spend the funds.
-     * @return A uint256 specifing the amount of tokens still avaible for the spender.
+     * @return A uint256 specifying the amount of tokens available for the spender.
      */
-    function allowance(address _owner, address _spender) public constant returns (uint256 remaining) {
-      return allowed[_owner][_spender];
+    function allowance(address _owner, address _spender) public view returns (uint256 remaining) {
+        return allowed[_owner][_spender];
     }
 }
 
